6a3959e443b7165050e0eff3917e948d11341375
James <jamesnjunge45@gmail.com>
2023-07-03 18:57:14 +0300

    updated

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 0-rectangle.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-"""Defines a Rectangle class."""
-
-
-class Rectangle:
-    """Represent a rectangle."""
-    pass
